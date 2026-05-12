52e8568ae0d3b5a59afa1b040f9dfcf0f125fd4e
Wolf Mermelstein <wolf@404wolf.com>
2026-05-11 20:58:28 -0400

    format

--- host/default.nix
+++ host/default.nix
@@ -49,10 +49,12 @@
     settings.Resolve.DNSStubListener = "yes";
   };
 
-  swapDevices = [{
-    device = "/swapfile";
-    size = 32768; # 32GB
-  }];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 32768; # 32GB
+    }
+  ];
 
   system.stateVersion = "23.11";
 }
